7dcb425032f36fe57a3da83ad80a83c9ba26ef3f
Matheus Juan <47642703+MatheusJuan@users.noreply.github.com>
2023-02-17 20:07:44 -0300

    f

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const chk = document.getElementById('chk');
 
 chk.addEventListener("click", () => {
-  document.getElementById('body').classList.toggle('body.light');
-  document.getElementById('btn').classList.toggle('btn.dark');
-  document.getElementById('title').classList.toggle('title.dark');
+  body.classList.toggle('body.light');
+  btn.classList.toggle('btn.dark');
+  title.classList.toggle('title.dark');
 });
