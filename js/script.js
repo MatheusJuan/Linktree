21b1af39717a6e99630884327198e95de1412a64
Matheus Juan <47642703+MatheusJuan@users.noreply.github.com>
2023-02-17 19:09:30 -0300

    1602

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const chk = document.getElementById('chk')
 
 chk.addEventListener('change', () => {
-  document.body.classList.toggle('light')
+  document.body.classList.toggle('light');
+  document.btn-primary.classList.toggle('light');
 })
