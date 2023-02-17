672901b0f40f47d543f0806dd3da7f55f097651b
Matheus Juan <47642703+MatheusJuan@users.noreply.github.com>
2023-02-17 20:34:54 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const chk = document.getElementById('chk');
-const body = document.getElementById(('.body');
-const btn = document.getElementById(('.btn-primary');
-const title = document.getElementById(('.title');
+const body = document.getElementById('body');
+const btn = document.getElementById('btn-primary');
+const title = document.getElementById('title');
 
 chk.addEventListener('change', () => {
   body.classList.toggle('light');
