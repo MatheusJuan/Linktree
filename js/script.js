db1b29d0605d432f397b3d0269eb47959d91b6a7
Matheus Juan <47642703+MatheusJuan@users.noreply.github.com>
2023-02-17 20:12:54 -0300

    light

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const title = document.getElementById('title');
 
 
 chk.addEventListener("change", () => {
-  body.classList.toggle('light');
+  body.classList.toggle('lightt');
   btn.classList.toggle('dark');
   title.classList.toggle('dark');
 });
