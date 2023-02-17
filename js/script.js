54d06aa2df48c724f9dc7bc842c314003a8b9f2a
Matheus Juan <47642703+MatheusJuan@users.noreply.github.com>
2023-02-17 19:44:23 -0300

    ded

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const chk = document.getElementById('chk');
+const change = document.getElementById("change");
 
-chk.addEventListener('click', function () {
+chk.change('click', function () {
   document.getElementById('body').toggle('dark')
   document.getElementById('btn').toggle('dark');
   document.getElementById('title').toggle('dark');
