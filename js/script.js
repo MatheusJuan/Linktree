06f88531041fab1d35610ec6fbd5f3c649fa7b0c
Matheus Juan <47642703+MatheusJuan@users.noreply.github.com>
2023-02-17 19:25:07 -0300

    1602

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 const chk = document.getElementById('chk');
 
-chk.addEventListener('change', function () {
+chk.addEventListener("change", function () {
   document.body.classList.toggle('light');
-  document.getElementById("btn").style.backgroundColor = chk.light;
-  document.getElementById("title").style.backgroundColor = chk.light;
 });
